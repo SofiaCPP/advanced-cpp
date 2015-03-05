bbe38f692500cb6cfbbb50771f4bc3f87d46c6b2
Dimitar Trendafilov <dimitar@coherent-labs.com>
2015-03-06 01:32:57 +0200

    * Added support for the `:cxx` filter
    * added custom jade render for hexo

---
+++ scripts/renderer-jade.js
@@ -0,0 +1,25 @@
+var jade = require('jade');
+
+var lt = /</g,
+    gt = />/g,
+    amp = /&/g,
+    quot = /"/g;
+
+function cxx(block) {
+    var escaped = block
+        .replace(amp, '&amp;')
+        .replace(lt, '&lt;')
+        .replace(gt, '&gt;')
+        .replace(quot, '&quot;');
+    return '<pre class="mstretch"><code>' + escaped + '</pre></code>';
+}
+
+jade.filters.cxx = cxx;
+
+var front = /(?:.*?)---(.*)/m;
+
+hexo.extend.renderer.register('jade', 'html', function(data, locals){
+    var m = front.exec(data.text);
+    return jade.compile(m? m[1] : data.text, {filename: data.path})(locals);
+}, true);
+
