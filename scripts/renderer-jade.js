600dd34f33b66add746ee08a68c34798da6b1048
Dimitar Trendafilov <dimitar@coherent-labs.com>
2015-03-15 02:53:11 +0200

    * ability to use more markdown for slides

--- scripts/renderer-jade.js
+++ scripts/renderer-jade.js
@@ -1,4 +1,37 @@
-var jade = require('jade');
+var jade = require('jade'),
+    mdit = require('markdown-it'),
+    mdit_container = require('markdown-it-container');
+
+function renderSlide(tokens, idx) {
+    if (tokens[idx].nesting === 1) {
+        return '<section>';
+    } else {
+        return '</section>';
+    }
+}
+
+var snippet = /^snippet\s+([^\s]+)\s*(.*)$/;
+
+function renderSnippet(tokens, idx) {
+    if (tokens[idx].nesting === 1) {
+        var m = tokens[idx].info.trim().match(snippet),
+            file = m[1],
+            sections = m[2];
+        return '<div class="snippet" data-file="' + file + '" data-sections="' +
+                sections +
+                '"><code><pre>// loading ... </pre></code>';
+    } else {
+        return '</div>';
+    }
+}
+
+var md = mdit().use(mdit_container, 'slide', { render: renderSlide, marker: '=' })
+               .use(mdit_container, 'snippet', { render: renderSnippet });
+
+function renderSlides(block) {
+    var r = md.render(block);
+    return r;
+}
 
 var lt = /</g,
     gt = />/g,
@@ -16,11 +49,13 @@ function cxx(block) {
 
 jade.filters.cxx = cxx;
 jade.filters.code = cxx;
+jade.filters.slides = renderSlides;
 
 var front = /(?:.*?)---(.*)/m;
 
-hexo.extend.renderer.register('jade', 'html', function(data, locals){
+function render(data, locals) {
     var m = front.exec(data.text);
     return jade.compile(m? m[1] : data.text, {filename: data.path})(locals);
-}, true);
+}
 
+hexo.extend.renderer.register('jade', 'html', render, true);
